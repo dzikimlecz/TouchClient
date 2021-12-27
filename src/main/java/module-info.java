a93129cfd3e8619dc7abebc2046155eb813e0faf
dzikimlecz <j.pul312@gmail.com>
2021-12-27 21:33:07 +0100

    added dependencies on jackson dotenv and cvurl

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,15 @@ module me.dzikimlecz.touchclient {
     requires com.dlsc.formsfx;
     requires javafx.graphics;
     requires org.jetbrains.annotations;
-//    requires dotenv.java;
+    requires dotenv.java;
+    requires coresearch.cvurl.io;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.datatype.jsr310;
 
     exports me.dzikimlecz.touchclient;
     opens me.dzikimlecz.touchclient.mainview to javafx.fxml;
+    opens me.dzikimlecz.touchclient.model to  com.fasterxml.jackson.databind;
+    opens me.dzikimlecz.touchclient.model.container to com.fasterxml.jackson.databind;
 }
